98fb192fe9f2a839325cce9a6668fc54bf327d93
Adrian Romero <adrian.romero@yoti.com>
2018-12-02 11:00:25 +0000

    Webapp: added initial components

---
+++ webapp/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ProductList from './ProductList';
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route path='/' exact={true} component={Home}/>
+                    <Route path='/products' exact={true} component={ProductList}/>
+                </Switch>
+            </Router>
+        )
+    }
+}
+
+export default App;
---
+++ webapp/src/ProductList.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { Button, ButtonGroup, Container, Table } from 'reactstrap';
+// import AppNavbar from './AppNavbar';
+import { Link } from 'react-router-dom';
+
+class ProductList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {products: [], isLoading: true};
+    }
+
+    componentDidMount() {
+        this.setState({isLoading: true});
+
+        fetch('/products')
+            .then(response => response.json())
+            .then(data => this.setState({products: data, isLoading: false}));
+    }
+
+    // async remove(id) {
+    //     await fetch(`/api/group/${id}`, {
+    //         method: 'DELETE',
+    //         headers: {
+    //             'Accept': 'application/json',
+    //             'Content-Type': 'application/json'
+    //         }
+    //     }).then(() => {
+    //         let updatedGroups = [...this.state.groups].filter(i => i.id !== id);
+    //         this.setState({groups: updatedGroups});
+    //     });
+    // }
+
+    render() {
+        const {products, isLoading} = this.state;
+
+        if (isLoading) {
+            return <p>Loading...</p>;
+        }
+
+        const productList = products.map(product => {
+            return <tr key={product.id}>
+                <td style={{whiteSpace: 'nowrap'}}>{product.name}</td>
+                <td style={{whiteSpace: 'nowrap'}}>{product.description}</td>
+                <td>
+                    <ButtonGroup>
+                        <Button size="sm" color="primary" tag={Link} to={"/orders/"}>Order</Button>
+                        {/*<Button size="sm" color="danger" onClick={() => this.remove(group.id)}>Delete</Button>*/}
+                    </ButtonGroup>
+                </td>
+            </tr>
+        });
+
+        return (
+            <div>
+                {/*<AppNavbar/>*/}
+                <Container fluid>
+                    <h3>Products</h3>
+                    <Table className="mt-4">
+                        <thead>
+                        <tr>
+                            <th width="20%">Name</th>
+                            <th width="20%">Description</th>
+                            <th width="10%">Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {productList}
+                        </tbody>
+                    </Table>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default ProductList;
