915feed6a47709fef717700579cebe6000cdc1fd
Adrian Romero <adrian.romero@yoti.com>
2019-01-03 16:51:23 +0100

    Security module

---
+++ server/security/src/main/java/com/adrrriannn/store/security/util/JWTConstants.java
@@ -0,0 +1,9 @@
+package com.adrrriannn.store.security.util;
+
+public final class JWTConstants {
+
+    private JWTConstants() {}
+
+    public static String HEADER_NAME = "token";
+    public static String TOKEN_PREFIX = "Authorization: Bearer ";
+}
